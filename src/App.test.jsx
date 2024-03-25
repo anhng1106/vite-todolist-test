7380572a8f15382d382ecace6e347a386cfb1189
Anh Nguyen <123630454+anhng1106@users.noreply.github.com>
2024-03-25 20:10:06 +0200

    add more test cases and test delete all button

---
+++ src/App.test.jsx
@@ -0,0 +1,45 @@
+import App from "./App";
+import { test, expect } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import TodoTable from "./TodoTable";
+
+test("renders App component", () => {
+  render(<App />);
+  const header = screen.getByText(/My Todolist/i);
+  expect(header).toBeInTheDocument();
+});
+
+test("renders todotable", () => {
+  const row = [{ desc: "Go to coffee", date: "24.01.2023" }];
+  render(<TodoTable todos={row} />);
+  const table = screen.getByRole("table");
+  expect(table).toHaveTextContent(/go to coffee/i);
+});
+
+test("add todo", () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "Go to coffee" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "29.01.2023" } });
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+  const table = screen.getByRole("table");
+  expect(table).toHaveTextContent(/go to coffee/i);
+});
+
+test("delete all todos", () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "Go to market" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "23.03.2023" } });
+  const buttonAdd = screen.getByText("Add");
+  fireEvent.click(buttonAdd);
+  const table = screen.getByRole("table");
+  expect(table).toHaveTextContent(/Go to market/i);
+
+  const button = screen.getByText("Delete All");
+  fireEvent.click(button);
+});
